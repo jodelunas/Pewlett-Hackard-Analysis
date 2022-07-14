96607944267c5885de2de063a7f4f10edf3176f6
Johannah Delunas <johannahdelunas@Johannahs-MacBook-Pro.local>
2022-07-13 19:11:57 -0500

    fixing mistake

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,9 +1,9 @@
 SELECT e.emp_no, e.first_name, e.last_name,
-t.title, t.from_date, t.to_date
+ti.title, ti.from_date, ti.to_date
 INTO retirement_titles
 FROM employees AS e
-INNER JOIN titles AS t
-on e.emp_no = t.emp_no
+INNER JOIN titles AS ti
+on e.emp_no = ti.emp_no
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
